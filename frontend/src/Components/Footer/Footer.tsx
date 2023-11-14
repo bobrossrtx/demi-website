f13aa573139716da533221dd2da34e185cee9a7a
bobrossrtx <bobrossrtx@gmail.com>
2023-11-14 14:47:16 +0000

    Added windows installer + footer

--- frontend/src/Components/Footer/Footer.tsx
+++ frontend/src/Components/Footer/Footer.tsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 
+import "./Footer.css"
+import { Link } from 'react-router-dom'
+
 type Props = {}
 
 type State = {}
@@ -9,7 +12,49 @@ export default class Footer extends Component<Props, State> {
 
   render() {
     return (
-      <div></div>
+      <footer>
+        <div className="w-100 mw-none ph3 mw8-m mw9-l center f3">
+          <div className="flex flex-col flex-row-ns pv0-1">
+            <div className="flex flex-col mw8 w-100 measure-wide-l pv2 pv5-m pv2-ns ph4-m ph4-l" id="get-help" data-np-autofill-form-type="other" data-np-checked="1" data-np-watching="1">
+              <h4>Get help!</h4>
+              <ul>
+                <li><Link to="/docs">Documentation</Link></li>
+                <li><Link to="/docs">Documentation</Link></li>
+                <li><Link to="https://discord.gg/vvhkhGkuGF">Ask a Question on the Discord Forum</Link></li>
+              </ul>
+            </div>
+            <div className="flex flex-col mw8 w-100 measure-wide-l pv2 pv5-m pv2-ns ph4-m ph4-l" id="get-help" data-np-autofill-form-type="other" data-np-checked="1" data-np-watching="1">
+              <h4>Terms & Policies</h4>
+              <ul>
+                <li><Link to="/policies/code-of-conduct">Code of Conduct</Link></li>
+                {/* <li><Link to="/policies/code-of-conduct">Licenses</Link></li> */}
+                <li><Link to="/policies/security">Security Notice</Link></li>
+                <li><Link to="/policies">All Policies</Link></li>
+              </ul>
+            </div>
+            <div className="flex flex-col mw8 w-100 measure-wide-l pv2 pv5-m pv2-ns ph4-m ph4-l" id="get-help" data-np-autofill-form-type="other" data-np-checked="1" data-np-watching="1">
+              <h4>Social</h4>
+              <div className="flex flex-row flex-wrap items-center footer-icons">
+                {/* The Discord link */}
+                <Link to="https://twitter.com/demiscript_off" target="_blank"><img src="/static/images/twitter.svg" alt="twitter logo" title="Twitter" /></Link>
+
+                {/* Add when the youtube channel is made */}
+                {/* <Link to="https://www.youtube.com/channel/UCaYhcUwRBNscFNUKTjgPFiA" target="_blank"><img className="pv2" src="/static/images/youtube.svg" alt="youtube logo" title="YouTube" /></Link> */}
+
+                {/* The discord link */}
+                <Link to="https://discord.gg/vvhkhGkuGF" target="_blank"><img src="/static/images/discord.svg" alt="discord logo" title="Discord" /></Link>
+
+                {/* Change when migrated to the Demi github org */}
+                <Link to="https://github.com/bobrossrtx/demi-lang" target="_blank"><img src="/static/images/github.svg" alt="github logo" title="GitHub" /></Link>
+              </div>
+            </div>
+          </div>
+          <div className="attribution">
+            <p>Maintained by Owen Boreham. See a bug? <Link to="https://github.com/bobrossrtx/demi-website/issues/new/choose">File an issue!</Link></p>
+            <p>©{new Date().getFullYear()} Demi Lang</p>
+          </div>
+        </div>
+      </footer>
     )
   }
 }
--- frontend/src/Routes/Downloads/Downloads.tsx
+++ frontend/src/Routes/Downloads/Downloads.tsx
@@ -8,17 +8,19 @@ type Props = {}
 export default function Downloads(props: Props) {
   return (
     <div className="container">
-      <h1>Download Deno <i className="fas fa-download"></i></h1>
+      <h1>Download Demi <i className="fas fa-download"></i></h1>
       <h2>Download the latest version for Windows | Linux | MacOS </h2>
       <hr className='hr-thick'/>
       <div className="current-version-container"></div><p className='current-version-label'>Current Demi Version: </p><p className="current-version">{process.env.REACT_APP_CURRENT_DEMI_VERSION}</p>
       <a href="https://github.com/bobrossrtx/demi-lang" className="github-link"><i className="fa fa-github"></i></a>
       <br></br>
       <div className="download-container">
+        <DownloadButton file={"/static/downloads/demi-lang-"+process.env.REACT_APP_CURRENT_DEMI_VERSION+"/demiscript-installer-"+process.env.REACT_APP_CURRENT_DEMI_VERSION+".exe"}>Windows Installer<i className="fa fa-download"></i></DownloadButton>
         <DownloadButton file={"/static/downloads/demi-lang-"+process.env.REACT_APP_CURRENT_DEMI_VERSION+"/bin/demi-lang-"+process.env.REACT_APP_CURRENT_DEMI_VERSION+"_windows.rar"}>Windows <i className="fa fa-windows"></i></DownloadButton>
         <DownloadButton file={"/static/downloads/demi-lang-"+process.env.REACT_APP_CURRENT_DEMI_VERSION+"/bin/demi-lang-"+process.env.REACT_APP_CURRENT_DEMI_VERSION+"_linux.zip"}>Linux <i className="fa fa-linux"></i></DownloadButton>
         <DownloadButton file={"/static/downloads/demi-lang-"+process.env.REACT_APP_CURRENT_DEMI_VERSION+"/bin/demi-lang-"+process.env.REACT_APP_CURRENT_DEMI_VERSION+"_macos.zip"}>MacOS <i className="fa fa-apple"></i></DownloadButton>
       </div>
+      <p>Disclaimer: The Windows installer may show an antivirus error as it doesn't yet have a valid signing key</p>
       <hr />
       <h2>Previous Versions</h2>
       <table className='release-table'>
