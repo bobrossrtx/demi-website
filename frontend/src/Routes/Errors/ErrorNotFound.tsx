8f33bacf5ab4a77ad58ddc02db158571ee77aa9a
Owen Boreham <bobrossrtx@gmail.com>
2025-03-18 14:09:58 +0000

    fix: update ErrorNotFound component styling and structure for better alignment

--- frontend/src/Routes/Errors/ErrorNotFound.tsx
+++ frontend/src/Routes/Errors/ErrorNotFound.tsx
@@ -10,7 +10,7 @@ export default function ErrorNotFound(props: Props) {
     const reason = queryParameters.get('reason')
 
     return (
-        <div className='container-text-center'>
+        <div className='container-error-center'>
             <h1 className='error-title'>404</h1>
             <p className='error-reason'>{reason ? reason : "Page not found"}</p>
         </div>
